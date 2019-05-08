3cc734b0a244138f536b0f77ace8b10d44823281
John M. Cook <john-cook@uiowa.edu>
2019-05-08 17:18:06 -0500

    added tables

---
+++ irl-stats/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("irl-stats.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irl",
+    version="0.0.1",
+    author="John M. Cook",
+    author_email="john-cook@uiowa.edu",
+    description="A collection of useful statistics packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uiowa-irl/irl-stats.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
